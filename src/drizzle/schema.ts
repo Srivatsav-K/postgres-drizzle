7a26931df7b706529610f9f1c9a9e1a96211b784
Srivatsav K <srivatsav@Srivatsavs-MacBook-Pro.local>
2024-04-24 12:29:45 +0530

    wip update user table schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,9 +1,14 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, integer, pgEnum } from "drizzle-orm/pg-core";
 
 // NOTE : export everything from this file
+
+export const UserRole = pgEnum("userRole", ["ADMIN", "BASIC"]);
 
 export const UserTable = pgTable("users", {
   id: serial("id").primaryKey(),
   // id2: uuid("id2").defaultRandom().primaryKey(),
   name: varchar("name", { length: 255 }).notNull(),
+  age: integer("age").notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  role: UserRole("role").default("BASIC").notNull(),
 });
