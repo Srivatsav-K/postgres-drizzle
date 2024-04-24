691ebecc241076560f862bf62b825cefb11c35ca
Srivatsav K <srivatsav@Srivatsavs-MacBook-Pro.local>
2024-04-24 23:45:54 +0530

    update schemas

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -6,6 +6,11 @@ import {
   integer,
   pgEnum,
   index,
+  uuid,
+  boolean,
+  timestamp,
+  real,
+  primaryKey,
 } from "drizzle-orm/pg-core";
 
 // NOTE : export everything from this file
@@ -21,6 +26,8 @@ export const UserTable = pgTable(
     age: integer("age").notNull(),
     email: varchar("email", { length: 255 }).notNull().unique(),
     role: UserRole("role").default("BASIC").notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
   },
   (table) => {
     return {
@@ -29,6 +36,52 @@ export const UserTable = pgTable(
 
       //If we want unique multiple fields (ex. a user should have unique name and age)
       uniqueNameAndAge: unique("uniqueNameAndAge").on(table.name, table.age),
+    };
+  }
+);
+
+export const UserPreferencesTable = pgTable("userPreferences", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  emailUpdates: boolean("emailUpdates").notNull().default(false),
+  userId: integer("userId")
+    .references(() => UserTable.id, { onDelete: "cascade" })
+    .notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+});
+
+export const PostTable = pgTable("posts", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: varchar("title", { length: 255 }).notNull(),
+  averageRating: real("averageRating").notNull().default(0), // float in sql; real in postgres
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+  authorId: integer("authorId")
+    .references(() => UserTable.id, { onDelete: "cascade" })
+    .notNull(),
+});
+
+export const CategoryTable = pgTable("categories", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 255 }).notNull(),
+});
+
+// Join table
+// id is not requried here as a combination of postId and categoryId can act as an id
+// We can use : composite primary key (primary key that uses multiple columns)
+export const PostCategory = pgTable(
+  "postCategory",
+  {
+    postId: uuid("postId")
+      .references(() => PostTable.id, { onDelete: "cascade" })
+      .notNull(),
+    categoryId: uuid("categoryId")
+      .references(() => CategoryTable.id, { onDelete: "cascade" })
+      .notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.postId, table.categoryId] }),
     };
   }
 );
